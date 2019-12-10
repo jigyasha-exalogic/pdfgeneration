38f8f2529a213ba6406a31be83379954a2bccc94
jigyasha-exalogic <jigyasha@exalogic.in>
2019-12-10 13:31:34 +0530

    5 commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   resources :sals, only: [:index, :create, :new, :update]
   get "/sals/:id/edit" => "sals#edit", as: "edit_sal"
   resources :accounts
-  resources :payslips, only: [:create, :show,:new]
+  resources :payslips, only: [:create, :show]
   delete '/destroy/:id' => "payslips#destroy", as: "destroy_payslip"
-  # get "/payslips/new/:id" => "payslips#new", as: "new_payslip"
+  get "/payslips/new/:id" => "payslips#new", as: "new_payslip"
   get "/payslips/:id/all" => "payslips#index", as: "show_all"
-  # get "/success/:id" => "payslips#show", as: "success"
+  get "/success/:id" => "payslips#show", as: "success"
   get "/payslips" => "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
