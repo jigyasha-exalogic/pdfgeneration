1f6de463778e5c43e7f79245f5c357cbe6893e6a
jigyasha-exalogic <jigyasha@exalogic.in>
2019-12-10 09:42:13 +0530

    2 Commit

--- db/migrate/20191209092023_change_basic.rb
+++ db/migrate/20191209092023_change_basic.rb
@@ -8,7 +8,6 @@ class ChangeBasic < ActiveRecord::Migration[6.0]
   	add_column :payslips, :cca, :integer, :default => 0
   	add_column :payslips, :lop, :integer, :default => 0
   	add_column :payslips, :od, :integer, :default => 0
-  	add_column :payslips, :pt, :integer, :default => 0
     add_column :payslips, :month, :integer
     add_column :payslips, :year, :integer
   end
