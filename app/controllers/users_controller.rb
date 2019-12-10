0f4ec6c9caa3acae28037d42f256be2aa93cbc02
jigyasha-exalogic <jigyasha@exalogic.in>
2019-12-10 15:30:41 +0530

    10 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  params.require(:user).permit(:email, :password, :password_confirmation, :fname, :lname, :age,:date_of_joining,:salary_per_annum, :salary_per_month, :phonenumber, :address, :blood_type, :emergency_name,:emergency_number, :primary_skill, :secondary_skill1, :secondary_skill2, :notice, :gender, :state, :pincode, :city, :country)
+  params.require(:user).permit(:email, :password, :password_confirmation, :fname, :lname, :age,:date_of_joining,:salary_per_annum, :salary_per_month, :phonenumber, :address, :blood_type, :emergency_name,:emergency_number, :primary_skill, :secondary_skill1, :secondary_skill2, :notice, :gender, :state, :pincode, :city, :country, :designation, :department)
   end
 
   def admin?
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,2 @@
 class Account < ApplicationRecord
-	validates :acname, format: {with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/}
 end
